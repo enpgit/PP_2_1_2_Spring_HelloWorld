08f650b057879a1460745359dd879d84d1013b03
Evgenij Pashkin <e.n.pashkin@gmail.com>
2023-01-28 23:51:20 +0300

    Add class Cat @Bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = applicationContext.getBean("getCat", Cat.class);
+        Cat cat2 = applicationContext.getBean("getCat", Cat.class);
+        boolean rez1 = bean == bean1;
+        boolean rez2 = cat1 == cat2;
+        System.out.println(rez1);
+        System.out.println(rez2);
     }
 }
